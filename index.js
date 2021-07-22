7b79e5d5ee8e8f328fbc4b9656a1d716d8c98598
John Ryan Cottam <jcottam@gmail.com>
2021-07-22 00:45:21 +0000

    added a Replit database

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-// const Database = require("@replit/database")
+const Database = require("@replit/database")
 const express = require('express');
 
 const app = express();
 
-// const db = new Database()
+const db = new Database()
 
-app.get('/', (req, res) => {
-  res.send('hi there')
+app.get('/', async (req, res) => {
+  const mySecret = process.env['API_KEY']
+  let count = await db.get("siteCount")
+  count += 1
+  console.log('count',count)
+  const newVal = await db.set("siteCount", count)
+  // const result = await db.get("newVal")
+  res.send('Site count: ' + count.toString())
 });
 
+app.get('/config',(req,res)=>{
+  const json = require('./config.json')
+  res.send(json)
+})
+
 app.listen(3000, () => {
-  console.log('server started');
+  // console.log('server started');
 });
